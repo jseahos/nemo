3822230db665dac1d5c4e4a7f8a68e7b38346217
Jordan Searcy-Hosea <jseahos@gmail.com>
2023-11-15 18:34:28 -1000

    Update main.cpp
    Refactor `main()` to use helper functions, to separate concerns.
    Register `r` as a command to reset the accumulator back to `0`.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <sstream>
 #include <string>
 
 using byte_t = uint8_t;
 static byte_t accumulator = 0;
+
+static std::optional<std::string> read_commands(const std::string& file);
+static void execute_commands(const std::string& commands);
 
 int main(int argc, char* argv[])
 {
@@ -14,18 +18,34 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    std::string commands;
-    {
-        std::stringstream buffer;
-        std::ifstream file(argv[1]);
-        if (!file) {
-            std::cerr << "nemo: error: failed to open '" << argv[1] << "'" << std::endl;
-            return -1;
-        }
-        buffer << file.rdbuf();
-        commands = buffer.str();
+    std::optional<std::string> commands = read_commands(argv[1]);
+    if (!commands.has_value()) {
+        std::cerr << "nemo: error: failed to open '" << argv[1] << "'" << std::endl;
+        return -1;
     }
 
+    execute_commands(commands.value());
+
+    return 0;
+}
+
+std::optional<std::string> read_commands(const std::string& file)
+{
+    std::optional<std::string> commands = {};
+
+    std::stringstream buffer;
+    std::ifstream stream(file);
+    if (!stream) {
+        return {};
+    }
+    buffer << stream.rdbuf();
+    commands = buffer.str();
+
+    return commands;
+}
+
+void execute_commands(const std::string& commands)
+{
     for (char command : commands) {
         switch (command) {
         case 'i':
@@ -36,6 +56,9 @@ int main(int argc, char* argv[])
             break;
         case 's':
             accumulator = (accumulator > 15 ? 0 : accumulator * accumulator);
+            break;
+        case 'r':
+            accumulator = 0;
             break;
         case 'o':
             std::cout << accumulator;
@@ -48,6 +71,4 @@ int main(int argc, char* argv[])
     /* Prevent zsh from appending a special newline to the output.
        https://unix.stackexchange.com/questions/167582/why-zsh-ends-a-line-with-a-highlighted-percent-symbol */
     std::cout << std::endl;
-
-    return 0;
 }
